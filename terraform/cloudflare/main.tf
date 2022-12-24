8a346461f4a68762f00a7f57599ff36014c9ba4e
Marco Mihai Condrache <52580954+MarcoMihaiCondrache@users.noreply.github.com>
2022-12-24 09:25:24 +0000

    repositories fix

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -1,13 +1,8 @@
 terraform {
-
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "3.30.0"
-    }
-    http = {
-      source  = "hashicorp/http"
-      version = "3.2.1"
+      version = "3.28.0"
     }
     sops = {
       source  = "carlpett/sops"
@@ -16,77 +11,25 @@ terraform {
   }
 }
 
-data "sops_file" "cloudflare_secrets" {
-  source_file = "secret.sops.yaml"
+data "sops_file" "settings" {
+    source_file = "secret.sops.yaml"
 }
 
 provider "cloudflare" {
-  email   = data.sops_file.cloudflare_secrets.data["cloudflare_email"]
-  api_key = data.sops_file.cloudflare_secrets.data["cloudflare_apikey"]
+    email = data.sops_file.settings.data["cloudflare_email"]
+    api_key = data.sops_file.settings.data["cloudflare_token"]
 }
 
 data "cloudflare_zones" "domain" {
   filter {
-    name = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
+    name = data.sops_file.settings.data["cloudflare_domain"]
   }
 }
 
-resource "cloudflare_zone_settings_override" "cloudflare_settings" {
+resource "cloudflare_record" "tunnel_route" {
+  name    = data.sops_file.settings.data["tunnel_route"]
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  settings {
-    ssl                      = "strict"
-    always_use_https         = "on"
-    min_tls_version          = "1.2"
-    opportunistic_encryption = "on"
-    tls_1_3                  = "zrt"
-    automatic_https_rewrites = "on"
-    universal_ssl            = "on"
-    browser_check            = "on"
-    challenge_ttl            = 1800
-    privacy_pass             = "on"
-    security_level           = "medium"
-    brotli                   = "on"
-    minify {
-      css  = "on"
-      js   = "on"
-      html = "on"
-    }
-    rocket_loader       = "on"
-    always_online       = "off"
-    development_mode    = "off"
-    http3               = "on"
-    zero_rtt            = "on"
-    ipv6                = "on"
-    websockets          = "on"
-    opportunistic_onion = "on"
-    pseudo_ipv4         = "off"
-    ip_geolocation      = "on"
-    email_obfuscation   = "on"
-    server_side_exclude = "on"
-    hotlink_protection  = "off"
-    security_header {
-      enabled = false
-    }
-  }
-}
-
-data "http" "ipv4" {
-  url = "http://ipv4.icanhazip.com"
-}
-
-resource "cloudflare_record" "ipv4" {
-  name    = "ipv4"
-  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = chomp(data.http.ipv4.response_body)
-  proxied = true
-  type    = "A"
-  ttl     = 1
-}
-
-resource "cloudflare_record" "root" {
-  name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
-  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  value   = "${data.sops_file.settings.data["tunnel_id"]}.cfargotunnel.com"
   proxied = true
   type    = "CNAME"
   ttl     = 1
