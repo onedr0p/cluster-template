e1330d734c8b766b1b39ccf870965e1d12c0b3da
Devin Buhl <onedr0p@users.noreply.github.com>
2024-01-21 11:39:12 -0500

    feat: more changes to templating (#1218)
    * feat: clean up more jinja default references
    
    Signed-off-by: Devin Buhl <devin@buhl.casa>
    
    * feat: add additional tls sans force kube-vip on k3s
    
    Signed-off-by: Devin Buhl <devin@buhl.casa>
    
    ---------
    
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- bootstrap/scripts/loader.py
+++ bootstrap/scripts/loader.py
@@ -20,19 +20,8 @@ class Loader:
     def __init__(self, data):
         if data.get("skip_tests", False):
             return
-        validation.validate_python_version()
-        validation.validate_cli_tools(data)
-        validation.validate_distribution(data)
-        validation.validate_github(data)
-        validation.validate_age(data)
-        validation.validate_timezone(data)
-        validation.validate_acme_email(data)
-        validation.validate_flux_github_webhook_token(data)
-        validation.validate_cloudflare(data)
-        validation.validate_host_network(data)
-        validation.validate_bootstrap_dns_server(data)
-        validation.validate_cluster_cidrs(data)
-        validation.validate_nodes(data)
+
+        validation.validate(data)
 
     def filters(self):
         return [nthhost, encrypt]
