f3190de74668dc55df1acd0d13aee98f7aab0825
Devin Buhl <devin@buhl.casa>
2024-01-21 17:18:21 -0500

    feat: update makejinja to 2.5.0 and implement mjfilter
    Signed-off-by: Devin Buhl <devin@buhl.casa>

---
+++ bootstrap/scripts/plugin.py
@@ -0,0 +1,52 @@
+from collections.abc import Callable
+from jinja2.utils import import_string
+from pathlib import Path
+from typing import Any
+import bcrypt
+import makejinja
+import netaddr
+
+import validation
+
+def nthhost(value: str, query: int) -> str:
+    value = netaddr.IPNetwork(value)
+    try:
+        nth = int(query)
+        if value.size > nth:
+            return str(value[nth])
+    except ValueError:
+        return False
+    return value
+
+def encrypt(value: str) -> str:
+    return bcrypt.hashpw(value.encode(), bcrypt.gensalt(rounds=10)).decode("ascii")
+
+class Plugin(makejinja.plugin.Plugin):
+    def __init__(self, data: dict[str, Any], config: makejinja.config.Config):
+        self.data = data
+        self.config = config
+
+        if self.data.get("skip_tests", False):
+            return
+
+        validation.validate(self.data)
+
+        self._excluded_dirs: set[Path] = set()
+        for input_path in config.inputs:
+            for filter_file in input_path.rglob(".mjfilter"):
+                filter_func: Callable[[dict[str, Any]], bool] = import_string(
+                    f"{filter_file}:main"
+                )
+                if filter_func(data) is False:
+                    self._excluded_dirs.add(filter_file.parent)
+
+    def filters(self) -> makejinja.plugin.Filters:
+        return [nthhost, encrypt]
+
+    def path_filters(self) -> makejinja.plugin.PathFilters:
+        return [self._mjfilter_func]
+
+    def _mjfilter_func(self, path: Path) -> bool:
+        return not any(
+            path.is_relative_to(excluded_dir) for excluded_dir in self._excluded_dirs
+        )
---
+++ bootstrap/templates/kubernetes/talos/.mjfilter
@@ -0,0 +1,1 @@
+main = lambda data: data["bootstrap_distribution"] == "talos"
