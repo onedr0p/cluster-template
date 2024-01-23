44911a996e665c129f3033a2eb3331cd1e05f3ed
Devin Buhl <devin@buhl.casa>
2024-01-23 14:44:19 -0500

    feat: more improvements to devcontainer
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- .devcontainer/ci/features/install.sh
+++ .devcontainer/ci/features/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+set -o noglob
 
 ARCH="$(uname -m)"
 if [ "$ARCH" = "x86_64" ]; then
@@ -13,29 +15,26 @@ fi
 
 apk add --no-cache \
     bash bind-tools ca-certificates curl python3 \
-    py3-pip moreutils jq git iputils \
-    openssh-client
+        py3-pip moreutils jq git iputils \
+            openssh-client
 
 apk add --no-cache \
     --repository=https://dl-cdn.alpinelinux.org/alpine/edge/community \
-    age direnv fish github-cli \
-    go-task helm k0sctl kubectl \
-    kustomize sops yq
-
-apk add --no-cache \
-    --repository=https://dl-cdn.alpinelinux.org/alpine/edge/testing \
-    cilium-cli flux stern
-
-# Naming things is hard
-ln -sf /usr/bin/go-task /usr/local/bin/task
+        age direnv fish helm kubectl kustomize sops yq
 
 #
 # Install Tools not in Alpine Repositories
 #
 
-curl https://i.jpillora.com/cloudflare/cloudflared! | bash
-curl https://i.jpillora.com/budimanjojo/talhelper! | bash
-curl https://i.jpillora.com/yannh/kubeconform! | bash
+curl -fsSL "https://i.jpillora.com/budimanjojo/talhelper!" | bash
+curl -fsSL "https://i.jpillora.com/cilium/cilium-cli!!?as=cilium" | bash
+curl -fsSL "https://i.jpillora.com/cli/cli!!?as=gh" | bash
+curl -fsSL "https://i.jpillora.com/cloudflare/cloudflared!" | bash
+curl -fsSL "https://i.jpillora.com/fluxcd/flux2!!?as=flux" | bash
+curl -fsSL "https://i.jpillora.com/go-task/task!" | bash
+curl -fsSL "https://i.jpillora.com/k0sproject/k0sctl!" | bash
+curl -fsSL "https://i.jpillora.com/stern/stern!" | bash
+curl -fsSL "https://i.jpillora.com/yannh/kubeconform!" | bash
 
 curl -fsSL -o /usr/local/bin/talosctl \
     "https://github.com/siderolabs/talos/releases/latest/download/talosctl-linux-${ARCH}"
