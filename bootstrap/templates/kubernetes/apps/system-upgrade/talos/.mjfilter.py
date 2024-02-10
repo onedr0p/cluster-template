2a09414ff451a673bc15f99550241d30a58c11d7
Wouter Bouvy <wouter@wouterbouvy.nl>
2024-02-10 21:06:47 +0000

    Talos: remove deprecated System Extentions and move to schematicID for talhelper and SUC

--- bootstrap/templates/kubernetes/apps/system-upgrade/talos/.mjfilter.py
+++ bootstrap/templates/kubernetes/apps/system-upgrade/talos/.mjfilter.py
@@ -2,6 +2,5 @@ main = lambda data: (
     data.get("distribution", {}).get("type", "k3s") in ["talos"] and
         data.get("distribution", {})
             .get("talos", {})
-            .get("schematics", {})
-            .get("enabled", False) == True
+            .get("schematicID", {})
 )
