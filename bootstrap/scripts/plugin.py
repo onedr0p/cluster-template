07f2d8e56e9e79d0f0af54ed8478045c4bb8b16e
Devin Buhl <devin@buhl.casa>
2024-06-03 21:48:36 -0400

    feat: implement talos_patches makejinja func
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- bootstrap/scripts/plugin.py
+++ bootstrap/scripts/plugin.py
@@ -11,6 +11,17 @@ import makejinja
 import validation
 
 
+def basename(value: str) -> str:
+    return Path(value).name
+
+
+def talos_patches(value: str) -> list[str]:
+    path = Path(f'bootstrap/templates/kubernetes/bootstrap/talos/patches/{value}')
+    if not path.is_dir():
+        return []
+    return [str(f) for f in path.glob('*') if f.is_file()]
+
+
 def nthhost(value: str, query: int) -> str:
     value = IPNetwork(value)
     try:
@@ -50,7 +61,11 @@ class Plugin(makejinja.plugin.Plugin):
 
 
     def filters(self) -> makejinja.plugin.Filters:
-        return [nthhost]
+        return [basename, nthhost]
+
+
+    def functions(self) -> makejinja.plugin.Functions:
+        return [talos_patches]
 
 
     def path_filters(self):
