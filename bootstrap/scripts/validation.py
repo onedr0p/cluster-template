5b8bb5ae35a657060de9ac05d88c1082784c7efa
Devin Buhl <onedr0p@users.noreply.github.com>
2024-11-06 20:15:56 -0500

    bootstrap: improve validation and update tasks (#1623)

--- bootstrap/scripts/validation.py
+++ bootstrap/scripts/validation.py
@@ -1,14 +1,11 @@
 from functools import wraps
-from shutil import which
-from typing import Callable, cast
-from zoneinfo import available_timezones
+from typing import Callable
+import dns.resolver
 import netaddr
+import ntplib
 import re
 import socket
 import sys
-
-GLOBAL_CLI_TOOLS = ["age", "flux", "helmfile", "sops", "jq", "kubeconform", "kustomize", "talosctl", "talhelper"]
-CLOUDFLARE_TOOLS = ["cloudflared"]
 
 
 def required(*keys: str):
@@ -19,83 +16,70 @@ def required(*keys: str):
                 if data.get(key) is None:
                     raise ValueError(f"Missing required key {key}")
             return func(*[data[key] for key in keys], **kwargs)
-
         return wrapper
-
     return wrapper_outter
 
 
 def validate_python_version() -> None:
     required_version = (3, 11, 0)
     if sys.version_info < required_version:
-        raise ValueError(f"Python {sys.version_info} is below 3.11. Please upgrade.")
-
-
-def validate_ip(ip: str) -> str:
-    try:
-        netaddr.IPAddress(ip)
-    except netaddr.core.AddrFormatError as e:
-        raise ValueError(f"Invalid IP address {ip}") from e
-    return ip
-
-
-def validate_network(cidr: str, family: int) -> str:
-    try:
-        network = netaddr.IPNetwork(cidr)
-        if network.version != family:
-            raise ValueError(f"Invalid CIDR family {network.version}")
-    except netaddr.core.AddrFormatError as e:
-        raise ValueError(f"Invalid CIDR {cidr}") from e
-    return cidr
+        raise ValueError(f"Invalid Python version {sys.version_info}, must be 3.11 or higher")
 
 
 def validate_node(node: dict, node_cidr: str) -> None:
-    if not node.get("name"):
-        raise ValueError(f"A node is missing a name")
-    if not re.match(r"^[a-z0-9-]+$", node.get('name')):
-        raise ValueError(f"Node {node.get('name')} has an invalid name")
-    if not node.get("disk"):
-        raise ValueError(f"Node {node.get('name')} is missing disk")
-    if not node.get("mac_addr"):
-        raise ValueError(f"Node {node.get('name')} is missing mac_addr")
-    if not re.match(r"(?:[0-9a-fA-F]:?){12}", node.get("mac_addr")):
-        raise ValueError(f"Node {node.get('name')} has an invalid mac_addr, is this a MAC address?")
-    if node.get("address"):
-        ip = validate_ip(node.get("address"))
-        if netaddr.IPAddress(ip, 4) not in netaddr.IPNetwork(node_cidr):
-            raise ValueError(f"Node {node.get('name')} is not in the node CIDR {node_cidr}")
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.settimeout(5)
-            result = sock.connect_ex((ip, 50000))
-            if result != 0:
-                raise ValueError(f"Node {node.get('name')} port 50000 is not open")
+    if not node.get('name') or not re.match(r"^[a-z0-9-]+$", node.get('name')):
+        raise ValueError(f"Invalid node name {node.get('name')} for {node.get('name')}, must be not empty and match [a-z0-9-]")
+    if not node.get('disk'):
+        raise ValueError(f"Invalid node disk {node.get('disk')} for {node.get('name')}, must be not empty")
+    if not node.get('mac_addr') or not re.match(r"(?:[0-9a-fA-F]:?){12}", node.get('mac_addr')):
+        raise ValueError(f"Invalid node mac_addr {node.get('mac_addr')} for {node.get('name')}, must be not empty and match [0-9a-fA-F]:?")
+    if node.get('schematic_id'):
+        if not re.match(r"^[a-z0-9]{64}$", node.get('schematic_id')):
+            raise ValueError(f"Invalid node schematic_id {node.get('schematic_id')} for {node.get('name')}, must match [a-z0-9]{64}")
+
+    try:
+        netaddr.IPAddress(node.get('address'))
+    except netaddr.core.AddrFormatError as e:
+        raise ValueError(f"Invalid IP address {node.get('address')}") from e
+
+    if netaddr.IPAddress(node.get('address'), 4) not in netaddr.IPNetwork(node_cidr):
+        raise ValueError(
+            f"Invalid node address {node.get('address')} for {node.get('name')}, must be in CIDR {node_cidr}"
+        )
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.settimeout(5)
+        result = sock.connect_ex((node.get('address'), 50000))
+        if result != 0:
+            raise ValueError(
+                f"Unable to connect to node {node.get('name')}, port 50000 is not connectable"
+            )
 
 
-@required("bootstrap_cloudflare")
-def validate_cli_tools(cloudflare: dict, **_) -> None:
-    for tool in GLOBAL_CLI_TOOLS:
-        if not which(tool):
-            raise ValueError(f"Missing required CLI tool {tool}")
-    for tool in CLOUDFLARE_TOOLS if cloudflare.get("enabled", False) else []:
-        if not which(tool):
-            raise ValueError(f"Missing required CLI tool {tool}")
+@required("bootstrap_cluster_name")
+def validate_cluster_name(name: str = "home-kubernetes", **_) -> None:
+    if not re.match(r"^[a-z0-9-]+$", name):
+        raise ValueError(f"Invalid bootstrap_cluster_name {name}, must be not empty and match [a-z0-9-]+")
 
 
-@required("bootstrap_age_pubkey")
-def validate_age(key: str, **_) -> None:
-    if not re.match(r"^age1[a-z0-9]{0,58}$", key):
-        raise ValueError(f"Invalid Age public key {key}")
+@required("bootstrap_schematic_id")
+def validate_schematic_id(id: str, **_) -> None:
+    if not re.match(r"^[a-z0-9]{64}$", id):
+        raise ValueError(f"Invalid bootstrap_schematic_id {id}, must be not empty and match [a-z0-9]{64}")
 
 
 @required("bootstrap_node_network", "bootstrap_node_inventory")
 def validate_nodes(node_cidr: str, nodes: dict[list], **_) -> None:
-    node_cidr = validate_network(node_cidr, 4)
+    try:
+        network = netaddr.IPNetwork(node_cidr)
+        if network.version != 4:
+            raise ValueError(f"Invalid bootstrap_node_network {network.version}, must be IPv4")
+    except netaddr.core.AddrFormatError as e:
+        raise ValueError(f"Invalid bootstrap_node_network {node_cidr}") from e
 
     controllers = [node for node in nodes if node.get('controller') == True]
-    if len(controllers) < 1:
-        raise ValueError(f"Must have at least one controller node")
-    if len(controllers) % 2 == 0:
-        raise ValueError(f"Must have an odd number of controller nodes")
+    if len(controllers) < 1 or len(controllers) % 2 == 0:
+        raise ValueError(f"Invalid number of controllers {len(controllers)}, must be odd and at least 1")
     for node in controllers:
         validate_node(node, node_cidr)
 
@@ -104,10 +88,43 @@ def validate_nodes(node_cidr: str, nodes: dict[list], **_) -> None:
         validate_node(node, node_cidr)
 
 
+@required("bootstrap_dns_servers")
+def validate_dns_servers(servers: list = ["1.1.1.1","1.0.0.1"], **_) -> None:
+    resolver = dns.resolver.Resolver()
+    resolver.nameservers = servers
+    resolver.timeout = 5
+    resolver.lifetime = 5
+
+    try:
+        resolver.resolve("cloudflare.com")
+    except Exception as e:
+        raise ValueError(f"Unable to resolve cloudflare.com with DNS servers {servers}") from e
+
+
+@required("bootstrap_ntp_servers")
+def validate_ntp_servers(servers: list = ["162.159.200.1","162.159.200.123"], **_) -> None:
+    client = ntplib.NTPClient()
+    for server in servers:
+        try:
+            client.request(server, version=3)
+        except Exception as e:
+            raise ValueError(f"Unable to connect to NTP server {server}") from e
+
+
+@required("bootstrap_age_pubkey")
+def validate_age(key: str, **_) -> None:
+    if not re.match(r"^age1[a-z0-9]{0,58}$", key):
+        raise ValueError(f"Invalid bootstrap_age_pubkey {key}, must be not empty and match age1[a-z0-9]{0,58}")
+
+
 def validate(data: dict) -> None:
     validate_python_version()
-    validate_cli_tools(data)
+    validate_cluster_name(data)
+    validate_schematic_id(data)
     validate_age(data)
 
-    if not data.get("skip_tests", False):
+    if not data.get('skip_tests', False):
         validate_nodes(data)
+
+    validate_dns_servers(data)
+    validate_ntp_servers(data)
