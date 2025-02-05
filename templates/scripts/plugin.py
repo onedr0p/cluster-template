c6123fce399a8b1c25b298e55e18d8e679c71251
Devin Buhl <onedr0p@users.noreply.github.com>
2025-02-04 21:06:37 -0500

    feat!: migrate flux ks out of flux-system (#1747)

--- templates/scripts/plugin.py
+++ templates/scripts/plugin.py
@@ -9,19 +9,13 @@ from netaddr import IPNetwork
 
 import makejinja
 import validation
+import re
+import json
 
 
 # Return the filename of a path without the j2 extension
 def basename(value: str) -> str:
     return Path(value).stem
-
-
-# Return a list of files in the talos patches directory
-def talos_patches(value: str) -> list[str]:
-    path = Path(f'templates/config/kubernetes/bootstrap/talos/patches/{value}')
-    if not path.is_dir():
-        return []
-    return [str(f) for f in sorted(path.glob('*.yaml.j2')) if f.is_file()]
 
 
 # Return the nth host in a CIDR range
@@ -34,6 +28,62 @@ def nthhost(value: str, query: int) -> str:
     except ValueError:
         return False
     return value
+
+
+# Return the age public key from age.key
+def age_public_key() -> str:
+    try:
+        with open('age.key', 'r') as file:
+            file_content = file.read()
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"File not found: age.key") from e
+    key_match = re.search(r"# public key: (age1[\w]+)", file_content)
+    if not key_match:
+        raise ValueError("Could not find public key in age.key")
+    return key_match.group(1)
+
+
+# Return the age private key from age.key
+def age_private_key() -> str:
+    try:
+        with open('age.key', 'r') as file:
+            file_content = file.read()
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"File not found: age.key") from e
+    key_match = re.search(r"(AGE-SECRET-KEY-[\w]+)", file_content)
+    if not key_match:
+        raise ValueError("Could not find private key in age.key")
+    return key_match.group(1)
+
+
+# Return cloudflare tunnel fields from cloudflared.json
+def cloudflare_tunnel(value: str) -> str:
+    try:
+        with open('cloudflared.json', 'r') as file:
+            try:
+                return json.load(file).get(value)
+            except json.JSONDecodeError as e:
+                raise ValueError(f"Could not decode cloudflare.json file") from e
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"File not found: cloudflared.json") from e
+
+
+# Return the GitHub deploy key from deploy.key
+def deploy_key() -> str:
+    try:
+        with open('deploy.key', 'r') as file:
+            file_content = file.read()
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"File not found: deploy.key") from e
+    return file_content
+
+
+# Return a list of files in the talos patches directory
+def talos_patches(value: str) -> list[str]:
+    path = Path(f'templates/config/kubernetes/bootstrap/talos/patches/{value}')
+    if not path.is_dir():
+        return []
+    return [str(f) for f in sorted(path.glob('*.yaml.j2')) if f.is_file()]
 
 
 def import_filter(file: Path) -> Callable[[dict[str, Any]], bool]:
@@ -64,15 +114,26 @@ class Plugin(makejinja.plugin.Plugin):
 
 
     def filters(self) -> makejinja.plugin.Filters:
-        return [basename, nthhost]
+        return [
+            basename,
+            nthhost
+        ]
 
 
     def functions(self) -> makejinja.plugin.Functions:
-        return [talos_patches]
+        return [
+            age_private_key,
+            age_public_key,
+            cloudflare_tunnel,
+            deploy_key,
+            talos_patches
+        ]
 
 
     def path_filters(self):
-        return [self._mjfilter_func]
+        return [
+            self._mjfilter_func
+        ]
 
 
     def _mjfilter_func(self, path: Path) -> bool:
