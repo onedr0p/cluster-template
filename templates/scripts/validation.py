63c2981e0ff194e30b2c342b67821ecbbe8accbb
Devin Buhl <devin@buhl.casa>
2025-02-06 10:01:32 -0500

    fix: add validation for github repository name
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- templates/scripts/validation.py
+++ templates/scripts/validation.py
@@ -103,6 +103,8 @@ def validate_ntp_servers(servers: list = ["162.159.200.1","162.159.200.123"], **
 
 @required("github")
 def validate_github_repository(github: dict, **_) -> None:
+    if not github.get('repository'):
+        raise ValueError(f"Missing required key github.repository")
     if not re.match(r"[a-zA-Z0-9-_]+/[a-zA-Z0-9-_]+", github.get('repository')):
         raise ValueError(f"Invalid github repository {github.get('repository')}, must match [a-zA-Z0-9-_]+/[a-zA-Z0-9-_]+")
 
