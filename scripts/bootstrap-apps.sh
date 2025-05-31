1f4c1f8ecbefb3f306b997795cab76fa12593e0e
Devin Buhl <onedr0p@users.noreply.github.com>
2025-05-31 09:55:24 -0400

    fix: check for TALOSCONFIG and KUBECONFIG envs in bootstrap

--- scripts/bootstrap-apps.sh
+++ scripts/bootstrap-apps.sh
@@ -128,6 +128,7 @@ function sync_helm_releases() {
 }
 
 function main() {
+    check_env KUBECONFIG TALOSCONFIG
     check_cli helmfile kubectl kustomize sops talhelper yq
 
     # Apply resources and Helm releases
