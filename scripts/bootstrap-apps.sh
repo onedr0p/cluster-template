b1e2aa9ed99591f2d0853281814abbd9687e96dd
Devin Buhl <onedr0p@users.noreply.github.com>
2025-10-21 07:29:25 -0400

    fix: path to cluster-secrets.sops.yaml

--- scripts/bootstrap-apps.sh
+++ scripts/bootstrap-apps.sh
@@ -60,7 +60,7 @@ function apply_sops_secrets() {
     local -r secrets=(
         "${ROOT_DIR}/bootstrap/github-deploy-key.sops.yaml"
         "${ROOT_DIR}/bootstrap/sops-age.sops.yaml"
-        "${ROOT_DIR}/kubernetes/components/common/sops/cluster-secrets.sops.yaml"
+        "${ROOT_DIR}/kubernetes/components/sops/cluster-secrets.sops.yaml"
     )
 
     for secret in "${secrets[@]}"; do
